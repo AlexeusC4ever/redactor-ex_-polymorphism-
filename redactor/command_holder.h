cda0e067f1613cf324d331555c30911a0593e541
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-08-09 15:19:18 +0300

    Update command_holder.h

--- redactor/command_holder.h
+++ redactor/command_holder.h
@@ -39,26 +39,3 @@ public:
 };
 
 #endif  //_COMMAND_HOLDER_H_
-
-//class Command_holder {
-//private:
-//	std::list<Command*> holder;
-//	std::list<Command*>::iterator command_pointer;
-//
-//public:
-//	Command_holder() :
-//		holder() {
-//		command_pointer = holder.end();
-//	}
-//	~Command_holder() {
-//		holder.clear();
-//	}
-//
-//	void push_back(Command* a);
-//
-//	Command* get_back();
-//
-//	Command* get_forward();
-//
-//	void clear_forward();
-//};
