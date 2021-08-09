b8585a78324d05b3df37a809f275ab89f320c38a
AlexeusC4ever <AlexeusC4ever>
2021-08-09 13:56:11 +0300

    overflow_command_solved

--- redactor/command_holder.cpp
+++ redactor/command_holder.cpp
@@ -1,8 +1,13 @@
 #include "command_holder.h"
 
 void Command_holder::push_back(Command* a) {
-	IsEmpty = false;
-	back.push(a);
+	back.push_back(a);
+	if (IsEmpty) {
+		IsEmpty = false;
+	}
+	else if (back.size() > overflow) {
+		back.erase(back.begin());
+	}
 }
 
 Command* Command_holder::get_forward() {
@@ -10,7 +15,7 @@ Command* Command_holder::get_forward() {
 		return nullptr;
 	}
 	Command* a = forward.top();
-	back.push(a);
+	back.push_back(a);
 	forward.pop();
 	return a;
 }
@@ -26,8 +31,37 @@ Command* Command_holder::get_back() {
 	if (back.empty()) { 
 		return nullptr;
 	}
-	Command* a = back.top();
+	Command* a = back.back();
 	forward.push(a);
-	back.pop();
+	back.erase(std::prev(back.end()));
 	return a;
 }
+
+
+
+//void Command_holder::push_back(Command* a) {
+//	holder.push_back(a);
+//	if (command_pointer != holder.end()) {
+//		++command_pointer;
+//	}
+//	else command_pointer = holder.begin();
+//}
+//
+//Command* Command_holder::get_forward() {
+//	return *(++command_pointer);
+//}
+//
+//void Command_holder::clear_forward() {
+//	auto buf = command_pointer;
+//	++buf;
+//	while (buf != holder.end()) {
+//		holder.erase(buf++);
+//	}
+//}
+//
+//Command* Command_holder::get_back() {
+//	if (command_pointer != holder.begin())
+//		return *(command_pointer--);
+//	else
+//		return*(command_pointer);
+//}
